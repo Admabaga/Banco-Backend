640cf9b363c2a963befa46c93d931a70829857f9
Admabaga <admabaga@outlook.com>
2024-09-16 16:32:03 -0500

    Refactor modificando la url del cliente web

--- src/main/java/com/example/Banco/Config/ConfigCors.java
+++ src/main/java/com/example/Banco/Config/ConfigCors.java
@@ -12,7 +12,7 @@ public class ConfigCors implements WebMvcConfigurer {
         @Override
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:5173") // Reemplaza por tu origen real
+                    .allowedOrigins("https://transcendent-dusk-034c79.netlify.app/") // Reemplaza por tu origen real
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("Content-Type", "Authorization")
                     .allowCredentials(true);
