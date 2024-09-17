3291896437922da4e57780210ab4e74b1e838e99
Admabaga <admabaga@outlook.com>
2024-09-16 23:57:35 -0500

    url

--- src/main/java/com/example/Banco/Config/ConfigCors.java
+++ src/main/java/com/example/Banco/Config/ConfigCors.java
@@ -12,7 +12,7 @@ public class ConfigCors implements WebMvcConfigurer {
         @Override
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**")
-                    .allowedOrigins("https://banantioquiaapp.netlify.app/") // Reemplaza por tu origen real
+                    .allowedOrigins("https://banantioquiaapp.netlify.app") // Reemplaza por tu origen real
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("Content-Type", "Authorization")
                     .allowCredentials(true);
