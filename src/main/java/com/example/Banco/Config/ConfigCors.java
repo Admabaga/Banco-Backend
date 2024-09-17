ac876c341a70d4b78fa3a29d9187a883483b37e3
Admabaga <admabaga@outlook.com>
2024-09-17 18:38:35 -0500

    Probando

--- src/main/java/com/example/Banco/Config/ConfigCors.java
+++ src/main/java/com/example/Banco/Config/ConfigCors.java
@@ -4,17 +4,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
 @Configuration
 public class ConfigCors implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("https://banco-front.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("Content-Type", "Authorization")
-                .allowCredentials(true);
+                .allowedHeaders("Content-Type", "Authorization", "X-Requested-With", "accept", "origin", "Access-Control-Request-Method",
+                        "Access-Control-Request-Headers")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
+
+
 
 
