61b0ae5abf580401c7b9a49796513f41a00ca9f5
Admabaga <admabaga@outlook.com>
2024-09-17 00:54:44 -0500

    config

--- src/main/java/com/example/Banco/Config/ConfigCors.java
+++ src/main/java/com/example/Banco/Config/ConfigCors.java
@@ -1,26 +1,18 @@
 package com.example.Banco.Config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.Arrays;
 
 @Configuration
 public class ConfigCors implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://banantioquiaapp.netlify.app")
+                .allowedOrigins("https://banantioquiaapp.netlify.app/") // Reemplaza por tu origen real
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
+                .allowedHeaders("Content-Type", "Authorization")
+                .allowCredentials(true);
     }
 }
