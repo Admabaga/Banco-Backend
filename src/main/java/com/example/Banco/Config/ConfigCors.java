77a3d895f2f94f4f09dcaf96d3fba3d4a7d8c563
Admabaga <admabaga@outlook.com>
2024-09-17 18:16:47 -0500

    Cambiando url del front para habilitar los cors

--- src/main/java/com/example/Banco/Config/ConfigCors.java
+++ src/main/java/com/example/Banco/Config/ConfigCors.java
@@ -10,7 +10,7 @@ public class ConfigCors implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://banantioquiaapp.netlify.app/") // Reemplaza por tu origen real
+                .allowedOrigins("https://banco-front.onrender.com/") // Reemplaza por tu origen real
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Content-Type", "Authorization")
                 .allowCredentials(true);
