9f5b1f9aa9381864295d20d31c0bf82476e4aae0
Admabaga <admabaga@outlook.com>
2024-09-25 17:06:33 -0500

    Refactor modificando cors

--- src/main/java/com/example/Banco/Config/ConfigCors.java
+++ src/main/java/com/example/Banco/Config/ConfigCors.java
@@ -6,21 +6,18 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class ConfigCors {
+public class ConfigCors implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer ConfigureCors() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*"); // Permite todos los headers
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
+
 
 //
 //    @Override
