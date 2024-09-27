e01b934adc4971cd8b2f2511c49272b9787d756d
Admabaga <admabaga@outlook.com>
2024-09-27 11:41:27 -0500

    Feat inicializando cors

--- src/main/java/com/example/Banco/BancoApplication.java
+++ src/main/java/com/example/Banco/BancoApplication.java
@@ -1,9 +1,12 @@
 package com.example.Banco;
 
+import com.example.Banco.Config.ConfigCors;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(ConfigCors.class)
 public class BancoApplication {
 
 	public static void main(String[] args) {
